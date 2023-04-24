e0f507e8167ebe399cc97ff3668151708f237abd
Thiago Nascimento <thiago_gja27@hotmail.com>
2023-04-24 13:57:15 -0300

    fix: limitação de casas decimais

--- main.js
+++ main.js
@@ -54,7 +54,7 @@ corpoTabela.innerHTML =linhas;
 
 function atualizaMediaFinal(){
     const mediaFinal = calculamediaFinal()
-    document.getElementById('media-final-valor').innerHTML = mediaFinal
+    document.getElementById('media-final-valor').innerHTML = mediaFinal.toFixed(2)
     document.getElementById('media-final-resultado').innerHTML = mediaFinal >= notaMinima ? spanAprovado : spanReprovado;
 
     
